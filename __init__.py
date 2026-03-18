1cf4a99d4b72e1455691812249d5a88e6349b2e8
veronesi <veronesi@ihp-microelectronics.com>
2026-03-18 12:06:02 +0100

    disabled imagenet

--- __init__.py
+++ __init__.py
@@ -1,10 +1,10 @@
 
-from .classification import mnist, fashionmnist, cifar10, cifar100, imagenet
+# from .classification import mnist, fashionmnist, cifar10, cifar100, imagenet
+from .classification import mnist, fashionmnist, cifar10, cifar100
 
 __all__ = [
     "mnist",
     "fashionmnist",
     "cifar10",
     "cifar100",
-    "imagenet"
 ]
--- classification/__init__.py
+++ classification/__init__.py
@@ -3,12 +3,11 @@ from . import mnist
 from . import fashionmnist
 from . import cifar10
 from . import cifar100
-from . import imagenet
+# from . import imagenet
 
 __all__ = [
     "mnist",
     "fashionmnist",
     "cifar10",
     "cifar100",
-    "imagenet"
 ]
